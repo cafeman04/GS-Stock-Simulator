44ada5c4e61c7e3502a8935817a73a7a9faabfe3
cafeman04 <krishpatel0405@gmail.com>
2024-09-08 19:21:06 -0400

    First commit

---
+++ stocksheet_auto.py
@@ -0,0 +1,52 @@
+import gspread
+import requests
+import time
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+#Google API Credentials
+scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json',scope)
+client = gspread.authorize(creds)
+sheet = client.open('StockSheet').sheet1
+
+#Creating companies with tickers and cell locations
+class Company():
+    def __init__(self, ticker,week0, week1, week2, week3, week4):
+        self.ticker = ticker
+        self.week0 = week0
+        self.week1 = week1
+        self.week2 = week2
+        self.week3 = week3
+        self.week4 = week4
+
+apple = Company('AAPL', [7,5], [7,8], [7,9], [7,10], [7,11])
+microsoft = Company('MSFT',[8,5], [8,8], [8,9], [8,10], [8,11])
+nvidia = Company('NVDA',[9,5],[9,8], [9,9], [9,10], [9,11])
+airbnb = Company('ABNB',[10,5],[10,8], [10,9], [10,10], [10,11])
+intuit = Company('INTU', [11,5],[11,8], [11,9], [11,10], [11,11])
+    
+dates = ["2024-02-26", "2024-03-01", "2024-03-08", "2024-03-15", "2024-03-21"]
+companies = [apple,microsoft,nvidia,airbnb,intuit]
+
+#Close Price Function - uses 2 APIs because first one doesn't work sometimes
+def find_close(stock, date):
+    response = requests.get(f'https://api.polygon.io/v1/open-close/{stock}/{date}?adjusted=true&apiKey=ajgfYm0WoHkAJdbKFOeaZ9p_VDuWtMKp').json()
+    if response['status'] == "NOT_FOUND":
+        response = requests.get(f'https://api.stockdata.org/v1/data/eod?symbols={stock}&date={date}&sort=True&api_token=6cXsTiEjHY7cB46slYlPoyt4SbYIYv6rbjGuu2Fu').json()
+        return str(response['data'][0]['close'])
+    else:
+        return str(response['close'])
+
+        
+#Loops through each company and each date for each cell
+tick = 0
+for company in companies:
+    for date in dates:
+        week = "week" + str(tick)
+        sheet.update_cell(getattr(company, week)[0], getattr(company, week)[1], find_close(company.ticker, date))
+        time.sleep(13)
+        tick += 1
+    tick = 0
+
+
---
+++ stockv2.py
@@ -0,0 +1,46 @@
+import requests
+
+stocks = []
+
+stock_input = input("Which stock do you want to see?: ")
+stocks.append(stock_input)
+while True:
+    repeater = input("Any other stocks or 'CONTINUE'?: ")
+    if repeater.upper() == 'CONTINUE':
+        break
+    else:
+        stocks.append(repeater)
+
+buy_date = '2023-05-04'
+# input(f"What date do you want to buy them? (YYYY-MM-DD): ")
+sell_date = '2023-06-05'
+# input(f"What date do you want to sell them? (YYYY-MM-DD): ")
+shares = 5
+# input("How many stocks do you want to buy?: ")
+
+
+def find_close(stock, date):
+    response = requests.get(f'https://api.polygon.io/v1/open-close/{stock}/{date}?adjusted=true&apiKey=ajgfYm0WoHkAJdbKFOeaZ9p_VDuWtMKp').json()
+    return response['close']
+
+class Company():
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.profit = round((find_close(ticker, sell_date) * shares - find_close(ticker, buy_date) * shares), 2)
+
+for company in stocks:
+    apple = Company(company)
+
+print(apple.profit)
+
+
+
+
+        
+
+
+
+
+
+
+
